97f8e135b0fb6100aea22d759a7658613214ce50
zxcvbn6622tw <55772606+zxcvbn6622tw@users.noreply.github.com>
2019-11-20 12:00:43 +0800

    程式

---
+++ Unity Project/Assets/Scripts/car.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class car : MonoBehaviour
+{
+    [Header("CC數"), Range(2000, 2500), Tooltip("拉")]
+    public int CC = 2500 ;
+    [Header("重量"), Range(1, 300)]
+    public float Weight = 300;
+   public string fac = "toyota";
+   public bool music = false ;
+
+}
+
---
+++ Unity Project/Assets/Scripts/player.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour
+
+{
+    [Header("玩家") ]
+    public string run;
+    public string jump;
+    public string attack;
+    public string hit;
+    [Header("HP") , Range(0,100) ]
+    public int hp;
+
+    [Header("道具"), Range(0,10)]
+    public int demand ;
+
+
+
+    }
+
