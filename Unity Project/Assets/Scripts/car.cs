a25605c3654322ad0700f02f68df8514c98f5c38
zxcvbn6622tw <55772606+zxcvbn6622tw@users.noreply.github.com>
2019-11-27 12:04:32 +0800

    傳回

--- Unity Project/Assets/Scripts/car.cs
+++ Unity Project/Assets/Scripts/car.cs
@@ -9,5 +9,40 @@ public class car : MonoBehaviour
    public string fac = "toyota";
    public bool music = false ;
 
+
+    private void Start()
+    {
+        print("是在哈囉");
+        print(CC);
+        print(Weight);
+        print(fac);
+
+        CC = 2000;
+    }
+    private void Update()
+    {
+        Drive(100,"左",effect:"火花");
+        int t = tax();
+        print("稅金:" + t);
+       
+    }
+
+    public void Drive (int speed,string direction,string sound="耶耶耶",string effect="速度線 ")
+    {
+        print("開車，時數:" + speed) ;
+        print("目前方向:" + direction);
+        print("聲音:"+sound);
+        print("特效:" + effect);
+    }
+
+    public int tax()
+    {
+        return CC * 100;
+    }
+
+
+
+
 }
+
 
--- Unity Project/Assets/Scripts/player.cs
+++ Unity Project/Assets/Scripts/player.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 public class player : MonoBehaviour
 
 {
-    [Header("玩家") ]
-    public string run;
-    public string jump;
-    public string attack;
-    public string hit;
-    [Header("HP") , Range(0,100) ]
-    public int hp;
+    [Header("玩家")]
+    public string run = "跑步開關";
+    public string jump = "跳躍開關";
+    public string attack = "攻擊觸發";
+    public string hit = "受傷觸發";
+    public string dead = "死亡";
 
-    [Header("道具"), Range(0,10)]
-    public int demand ;
+    [Header("需要道具")]
+    public int demand = 10;
+    [Header("現有道具")]
+    public int have = 0;
+    [Header("數值")]
+    public bool die=false;
+    [Range(0,200)]
+    public float hp = 100;
+
+
+    
 
 
 
-    }
+}
 
