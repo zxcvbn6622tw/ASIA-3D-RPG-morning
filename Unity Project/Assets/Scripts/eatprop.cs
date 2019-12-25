2619de0a1dc8c890cd3f8572900cbf9dfb43a9a9
zxcvbn6622tw <55772606+zxcvbn6622tw@users.noreply.github.com>
2019-12-25 11:53:19 +0800

    網頁版

---
+++ Unity Project/Assets/Scripts/eatprop.cs
@@ -0,0 +1,55 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class eatprop : MonoBehaviour
+{
+    [Header("寶相數量介面")]
+    public Text textcount;
+    [Header("提示畫面")]
+    public GameObject objTip;
+
+    public int propCount;
+    public int propTotal = 5;
+    public bool finish;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "寶箱")
+        {
+            Destroy(collision.gameObject);
+
+
+            propCount++;
+            textcount.text = "寶箱:" + propCount + "/5";
+
+            if (propCount == propTotal)
+            {
+                finish = true;
+                objTip.SetActive(true);
+            }
+        }
+       
+        }
+
+    private void Update()
+    {
+        Gameover();
+    }
+
+    private void Gameover()
+    {
+        if (finish)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            { Application.Quit(); }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("場景");
+            }
+        }
+
+    }
+}
